c70358fbdb95dca3bdbc3a2a804d0bf06fd12fb0
inode-001 <furyfish7@proton.me>
2024-04-11 05:04:05 -0500

    first version

--- metadata_eraser.sh
+++ metadata_eraser.sh
@@ -1,16 +1,26 @@
-file=$1
-if [[  -f $file  ]] ; then
-      echo "Would you want to backup the picture before wiping its metadata"
+if [[  $# == 0 ]] ; then
+       echo -e "\e[31mPlease Input File.\e[0m\n Usage :\n\t\e[32mbash $0 yourfile\e[0m"
+       exit
+fi
+
+if [[  -f $1 ]] ; then
+      echo "Would you want to backup the picture before wiping its metadata: "
       select option in YES NO QUIT ; do
           case $option in
                 YES )
                    exiftool -all= "$1"
                    ;;
                   NO )
-                     exiftool -all= -overwrite_original "$1"
+                     exiftool -all= -overwrite_original "$1";;
+                  QUIT )
+                  echo -e "\e[33mQuitting......\e[0m";;
            esac
            
            exit
       
-      done
-fi          
+      done        
+else
+   echo -e "\e[31mFILE NOT FOUND\e[0m"
+fi
+
+
